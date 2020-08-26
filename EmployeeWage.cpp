fe10d5c7813afe7a3173cc5440855cdb444f27ae
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-26 09:01:48 +0530

    ADDED Code to calculate daily employee wages.

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -7,15 +7,21 @@ using namespace std;
 class EmployeeWage
 {
     int present = 1;
+    const int EMP_RATE_PER_HOUR = 20;
+
     public:
         void getAttendance()
         {
+            int empHrs = 0;
+            int empWage = 0;
             srand(time(0));
             int empCheck = rand() % 2;
             if (empCheck == present)
-                cout << "Employee is Present" << endl;
+                empHrs = 8;
             else
-                cout << "Employee is Absent" << endl;
+                empHrs = 0;
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            cout << "Employee Wage Per Day = " << empWage << endl;  
         }
 };
 
