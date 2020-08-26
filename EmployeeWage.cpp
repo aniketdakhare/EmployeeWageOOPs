0c6937110fb6c35c3c273c0ac6618c6befdffe28
“Aniket_D” <aniketdakhare@gmail.com>
2020-08-26 09:17:32 +0530

    ADDED Code to calculate employe wage for part time employee.

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -4,29 +4,44 @@
 
 using namespace std;
 
-class EmployeeWage
+class EmployeeWageComputation
 {
-    int present = 1;
     const int EMP_RATE_PER_HOUR = 20;
 
     public:
-        void getAttendance()
-        {
-            int empHrs = 0;
-            int empWage = 0;
-            srand(time(0));
-            int empCheck = rand() % 2;
-            if (empCheck == present)
-                empHrs = 8;
-            else
-                empHrs = 0;
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            cout << "Employee Wage Per Day = " << empWage << endl;  
-        }
+        int getWorkingHours();
+        void getEmployeeWage();
 };
+
+int EmployeeWageComputation :: getWorkingHours()
+{
+    const int PART_TIME = 1;
+    const int FULL_TIME = 2;   
+    int empHrs = 0;
+    srand(time(0));
+    int empCheck = rand() % 3;
+    switch(empCheck)
+    {
+        case PART_TIME:
+            empHrs = 4;
+            break;
+        case FULL_TIME:
+            empHrs = 8;
+            break;
+        default:
+            empHrs = 0;
+    }
+    return empHrs;
+}
+
+void EmployeeWageComputation :: getEmployeeWage()
+{
+    int empWage = getWorkingHours() * EMP_RATE_PER_HOUR;
+    cout << "Employee Wage Per Day = " << empWage << endl;    
+}
 
 int main()
 {
-    EmployeeWage* empWage = new EmployeeWage;
-    empWage->getAttendance();  
+    EmployeeWageComputation* empWage = new EmployeeWageComputation;
+    empWage->getEmployeeWage();  
 }
